e58eb4414ff993cd442318c688215c41c00be634
griffithjj <30388482+griffithjj@users.noreply.github.com>
2017-07-23 16:46:05 +0200

    Create hello.cpp
    Hello world c++

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+
+  cout << endl << "Hello World!!!" << endl;
+  
+  return 0;
+  
+}//Main function
